f6b3c0112e81be806ae32c707c94dc9072e5cef7
Luz Gomez <luz.torrente2020@gmail.com>
2021-07-07 04:51:50 -0500

    Se incluye mensaje sobre el proyecto

--- src/main/java/co/edu/utp/misiontic2022/c2/Main.java
+++ src/main/java/co/edu/utp/misiontic2022/c2/Main.java
@@ -1,5 +1,9 @@
 package co.edu.utp.misiontic2022.c2;
-
+/** Creacion de proyecto Maven para ejercicios Clase 04lases 
+ *  Creacion clases necesarias para modelar ejercicio de ajedrez
+ *  La clase con funcion main() solo se usa para hecr llamadas
+ *  a las funciones que se van a probar
+ */
 public class Main {
     public static void main(String[] args) {
         
